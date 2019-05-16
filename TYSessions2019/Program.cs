0db9a8ae66b998f459e6ffee03d7241d4c332a44
Dylan Egan <itsty2019degan@outlook.com>
2019-05-16 15:46:34 +0100

    Sofware Session Over

--- TYSessions2019/Program.cs
+++ TYSessions2019/Program.cs
@@ -12,7 +12,18 @@ namespace TYSessions2019
          
     static void Main(string[] args)
         {
-            
+            Activity.Track("Excercise 1 Adding two numbers together");
+            int num1, num2, result;
+            Console.WriteLine("Enter First Number followed by return");
+            num1 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("You entered {0}", num1);
+            Console.WriteLine("Enter Second Number followed by return");
+            num2 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("You entered {0}", num2);
+            result = num1 + num2;
+            Console.WriteLine("The result of {0} + {1} is {2}", num1, num2, result);
+            Console.WriteLine("Press any Key to Exit");
+            Console.ReadKey();
         }
     }
 }
